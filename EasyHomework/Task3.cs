a394f4db165e9514127d4459a8a5bb9fba070f38
blackvenon <6186914@gmail.com>
2020-12-09 15:20:39 +0600

    update#2

--- EasyHomework/Task3.cs
+++ EasyHomework/Task3.cs
@@ -6,22 +6,105 @@ namespace EasyHomework
 {
     class Task3
     {
+
+
+
+
+        public static void Solve(int A, int B, int C)
+        {
+
+            if (A != 0)
+            {
+                int D = B * B - 4 * A * C;
+                Console.WriteLine(D);
+                if (D < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Корней нет");
+
+                }
+                else if (D == 0)
+                {
+                    Console.WriteLine(D);
+                    int T = -B / (2 * A);
+                    Console.WriteLine(T);
+                    if (T > 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Первый Корень >>>>>>{Math.Sqrt(T)} Второй Корень>>>>>>{-Math.Sqrt(T)}");
+                    }
+                    else if (T == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(" Корень >>>>> 0");
+
+                    }
+                }
+                else if (D > 0)
+                {
+                    Console.WriteLine(D);
+                    double T1 = (-B + Math.Sqrt(D)) / (2 * A);
+                    double T2 = (-B - Math.Sqrt(D)) / (2 * A);
+                    Console.WriteLine(T1);
+                    if (T1 > 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Первый Корень >>>>>>{Math.Sqrt(T1)} Второй Корень>>>>>>{-Math.Sqrt(T1)}");
+                    }
+                    if (T2 > 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Первый Корень >>>>>>{Math.Sqrt(T2)} Второй Корень>>>>>>{-Math.Sqrt(T2)}");
+                    }
+
+                }
+
+                
+            }
+
+        }
+
+
+    
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Введите символ");
-             var rawch = Console.ReadLine();
-            if (char.TryParse(rawch, out  char ch))
-            {
-                var resultcode = (int)ch + 1;
-                Console.WriteLine((char)resultcode);
 
+
+            int A, B, C;
+            Console.WriteLine("Введите A");
+            while (!int.TryParse(Console.ReadLine(), out  A))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("ВВедите корректные данные!!!");
+                Console.ResetColor();
+            }
+            Console.WriteLine("Введите B");
+           
+            while (!int.TryParse(Console.ReadLine(), out B))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("ВВедите корректные данные!!!");
+                Console.ResetColor();
             }
             
-
-            else 
+            Console.WriteLine("Введите C");
+            while (!int.TryParse(Console.ReadLine(), out  C))
             {
-                Console.WriteLine("Некорректные данные");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("ВВедите корректные данные!!!");
+                Console.ResetColor();
             }
+
+
+
+            
+            Solve(A,B,C);
+
+          
+          
+
         }
     }
 }
+
--- MediumHomework/Task1.cs
+++ MediumHomework/Task1.cs
@@ -4,25 +4,93 @@ namespace MediumHomework
 {
 
 
-    enum Accounts
+
+    abstract class GeometryFigure
     {
-        Current,
-        Credit,
-        SocialPays,
-        Deposit
+
+        protected abstract double S();
+
+        public  void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+       
     }
 
 
+    class Rectangle : GeometryFigure
+    {
+        protected double Width { get; set; }
+        protected double Length { get; set; }
+
+        public Rectangle(double l, double w) { Length = l; Width = w; }
+
+        public override string ToString()
+        {
+            return   $"Ширина >>>>> {Width.ToString()} Длина >>>> {Length.ToString()} Площадь >>>>> {S()}";
+        }
+
+        protected override double S()
+        {
+
+            return Width * Length;
+
+
+        }
+
+
+
+        class Square : Rectangle
+        {
+            public Square(double a) : base(a, 0)
+            {
+                this.Length = a;
+            }
+
+
+            protected override double S()
+            {
+
+                return Length * Length;
+
+            }
+
+            public override string ToString()
+            {
+                return   $"сторона >>>> {Length.ToString()} Площадь >>>>> {S()}";
+            }
+
+        }
+
+
+        class Circle : GeometryFigure
+        {
+            protected double R;
+
+            public Circle(double r) { R = r; }
+
+            protected override double S()
+            {
+
+                return 3.14 * (R * R);
+
+            }
+
+            public override string ToString()
+            {
+                return   $"Радиус >>>> {R.ToString()} Площадь >>>>> {S()}";
+            }
+        }
+    }
 
 
     class Task1
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Accounts account;
-            account = Accounts.Credit;
-            Console.WriteLine(account);
-            
+            Rectangle rec = new Rectangle(5,8);
+            rec.Print();
+
         }
     }
 }
